ef0c22abcb9870aed5369c5c69379b3d3a0dc56a
MarcosStanquini <marcossnarquini@gmail.com>
2024-01-31 13:14:34 -0300

    feat: add topic

---
+++ app.py
@@ -0,0 +1,51 @@
+from selenium import webdriver
+from decouple import config
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from time import sleep
+import pandas as pd
+import numpy as np
+
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
+table = pd.read_excel('TopicsAndEntries.xlsx')
+for i,topic in enumerate(table['Topic']):
+    topics = table.loc[i, 'Topic']
+    entries = table.loc[i, 'Entries']
+
+    driver.get("http://127.0.0.1:8000/users/login")
+    username = driver.find_element(By.ID, "id_username")
+    password = driver.find_element(By.ID, "id_password")
+
+    username.send_keys(config("USERNAME_TEST"))
+    password.send_keys(config("PASSWORD"))
+    sleep(5)
+
+    login_attempt = driver.find_element(By.XPATH, '/html/body/form/button').click()
+
+    driver.find_element(By.XPATH, '/html/body/a[1]').click()
+    driver.find_element(By.XPATH, '/html/body/a[3]').click()
+    sleep(5)
+    input_entries = driver.find_element(By.ID, "id_text").send_keys(str(topics))
+    driver.find_element(By.XPATH, '/html/body/form/button').click()
+    sleep(5)
+    topic_xpath = f"//a[contains(text(), '{topics}')]"
+    driver.find_element(By.XPATH, topic_xpath).click()
+    sleep(5)
+    driver.find_element(By.XPATH, '/html/body/p[5]/a').click()
+    driver.find_element(By.XPATH, '//*[@id="id_text"]').click().send_keys(str(entries))
+    sleep(3)
+
+   
+    
+
+
+
+    driver.quit()
+
+
+
+
